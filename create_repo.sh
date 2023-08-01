89530254b987cde383280b234e07a4a567cb9ca9
Bartosz <bt_nowakowski@outlook.com>
2023-08-01 08:42:46 +0200

    FIRST COMMIT

---
+++ create_repo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+if [ $# != 1 ]; then
+	echo "        "
+	echo " NO PARAMETERS PROVIDED"
+	echo " ---------------------------------"
+	echo " Script takes a name of repository to be created as a parameter!"
+	echo " Usage: ./create_repo.sh new_repo_name"
+	echo " ---------------------------------"
+	exit 2;
+fi
+
+repo_name=$1
+directory=#path to directory where you want to create repo
+path=$directory/$repo_name
+mkdir $path
+git -C "$path" init 
+git -C "$path" config --global user.email "<YOUR EMAIL>"
+git -C "$path" config --global user.name "<YOUR NAME>"
+cd $path
+code .
