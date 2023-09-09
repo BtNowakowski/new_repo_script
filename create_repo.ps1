8cd2cb2e27db31108cf24d4e8ceb592be71949fa
BtNowakowski <107316656+BtNowakowski@users.noreply.github.com>
2023-09-09 16:08:52 +0200

    added powershell script

---
+++ create_repo.ps1
@@ -0,0 +1,45 @@
+param (
+    [string]$repo_name
+)
+
+if (-not $repo_name) {
+    Write-Host "        "
+    Write-Host " NO PARAMETERS PROVIDED"
+    Write-Host " ---------------------------------"
+    Write-Host " Script takes a name of repository to be created as a parameter!"
+    Write-Host " Usage: ./create_repo.ps1 new_repo_name"
+    Write-Host " ---------------------------------"
+    exit 2
+}
+
+$directory = "<PATH FOR REPOSITORIES>"
+$path = Join-Path -Path $directory -ChildPath $repo_name
+
+if (-not (Test-Path $directory -PathType Container)) {
+    Write-Host "Provided directory doesn't exist."
+    exit 2
+}
+
+New-Item -Path $path -ItemType Directory -Force
+Set-Location -Path $path
+New-Item -Name "source" -ItemType Directory
+New-Item -Name "README.md" -ItemType File
+New-Item -Name ".gitignore" -ItemType File
+New-Item -Name "requirements.txt" -ItemType File
+
+Add-Content -Path ".gitignore" -Value ".pytest_cache"
+Add-Content -Path ".gitignore" -Value ".vscode"
+Add-Content -Path ".gitignore" -Value "/source/__pycache__"
+
+Add-Content -Path "README.md" -Value "#$repo_name"
+
+# Initialize Git repository and configure user email and name
+git init
+git config user.email "<YOUR EMAIL>"
+git config user.name "<YOUR NAME>"
+
+# Add all files to the initial commit
+git add .
+git commit -m "Initial commit"
+
+Invoke-Expression "code ."
